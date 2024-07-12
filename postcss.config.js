95592b0d4b5ecddcb73e99275ff1dd25a8a7f144
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-07-12 15:36:53 +0300

    PostCSS setup

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
+import postCSSSortMediaQueries from "postcss-sort-media-queries";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [
+    purgeCSSPlugin({
+      content: ["./**/*.html"],
+      extractors: [
+        {
+          extractor: (content) => {
+            return content.match(/[A-z0-9-:\/]+/g);
+          },
+          extensions: ["html"],
+        },
+      ],
+    }),
+    postCSSSortMediaQueries(),
+    autoprefixer(),
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,3 @@
 import { defineConfig } from "vite";
-import postcss from "postcss";
-import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
-import postCSSSortMediaQueries from "postcss-sort-media-queries";
 
-export default defineConfig({
-  plugins: [
-    // postcss({
-    //   plugins: [
-    //     purgeCSSPlugin({
-    //       content: ["./*.html"],
-    //     }),
-    //     postCSSSortMediaQueries()
-    //   ],
-    // }),
-  ],
-});
+export default defineConfig({});
